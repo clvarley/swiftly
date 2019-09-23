9bcc71d2ab840ee10fa8b4d43cee2d30b5cbb26d
clvarley <conorlukevarley@gmail.com>
2019-09-23 21:42:40 +0100

    Initial router API work

--- src/swiftly/routing/Router.php
+++ src/swiftly/routing/Router.php
@@ -16,19 +16,47 @@ Class Router
   private $routes = [];
 
   /**
+   * Builds a router for the routes provided
    *
+   * @param array $routes [Optional] Route definitions
    */
   public function __construct( array $routes = [] )
   {
-
+    $this->routes = $routes;
   }
 
   /**
+   * Create a router from a JSON route file
    *
+   * @param string $filepath  Path to JSON file
+   * @return Router           Router object
    */
-  public function get()
+  public static function fromJson( string $filepath ) : Router
   {
+    $values = [];
 
+    if ( is_file($filepath) && ( $values = file_get_contents($filepath) ) !== false ) {
+
+      $values = json_decode($filepath, true);
+
+      if ( json_last_error() !== JSON_ERROR_NONE ) {
+        $values = [];
+      }
+    }
+
+    return ( new Router($values) );
+  }
+
+  /**
+   * Gets the callable for the route specified
+   *
+   * @param string $route The route
+   * @return callable|null
+   */
+  public function get() : ?callable
+  {
+    // TODO:
+    return [$this, 'get'];
   }
 
 }
