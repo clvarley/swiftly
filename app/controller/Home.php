7ebad4814150f947dd19bce6be5c251ea162e6e6
clvarley <conorlukevarley@gmail.com>
2019-09-27 22:44:37 +0100

    Added example controller and view

---
+++ app/controller/Home.php
@@ -0,0 +1,24 @@
+<?php
+
+use Swiftly\Base\Controller;
+
+/**
+ * The default controller that handles the homepage
+ *
+ * @author C Varley <cvarley@highorbit.co.uk>
+ */
+Class Home Extends Controller
+{
+
+  /**
+   * Load the homepage template
+   */
+  public function index()
+  {
+    $this->setOutput($this->render('home', [
+      'title'   => 'Swiftly | A Simple Framework',
+      'message' => 'Thanks for installing Swiftly!'
+    ]));
+  }
+
+}
---
+++ app/view/home.html.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title><?= $this->title ?></title>
+  </head>
+  <body>
+
+    <h1><?= $this->title; ?></h1>
+    <p><?= $this->message; ?></p>
+
+  </body>
+</html>
