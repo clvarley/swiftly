e1a9da803b0591bd553f3de13330717b761746fe
clvarley <conorlukevarley@gmail.com>
2019-11-14 12:54:35 +0000

    Foundations for Mysql, PostgreSQL and SQLite drivers

---
+++ src/swiftly/database/adapters/Mysql.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Swiftly\Database\Adapters;
+
+use \Swiftly\Database\AdapterInterface;
+
+/**
+ * Driver for MySQL databases
+ *
+ * @author C Varley <cvarley@highorbit.co.uk>
+ */
+Class Mysql Implements AdapterInterface
+{
+
+}
---
+++ src/swiftly/database/adapters/Sqlite.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Swiftly\Database\Adapters;
+
+use \Swiftly\Database\AdapterInterface;
+
+/**
+ * Driver for SQLite databases
+ *
+ * @author C Varley <cvarley@highorbit.co.uk>
+ */
+Class Sqlite Implements AdapterInterface
+{
+
+}
