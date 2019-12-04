bf045ff4bea6ab7f4504ed12fa5fd42517045623
clvarley <conorlukevarley@gmail.com>
2019-12-04 13:27:43 +0000

    Added method stubs for MySQL adapter

--- src/swiftly/database/adapters/Mysql.php
+++ src/swiftly/database/adapters/Mysql.php
@@ -12,4 +12,78 @@ use \Swiftly\Database\AdapterInterface;
 Class Mysql Implements AdapterInterface
 {
 
+    /**
+     * Handle to the MySQL DB
+     *
+     * @var \mysqli $handle DB handle
+     */
+    private $handle = null;
+
+    /**
+     * Results of last query
+     *
+     * @var array $results Query results
+     */
+    private $results = [];
+
+    /**
+     * Opens a connection to the MySQL database
+     *
+     * @return bool Connection opened
+     */
+    public function open() : bool
+    {
+        $this->handle = new mysqli();
+
+        return true;
+    }
+
+    /**
+     * Executes the given query
+     *
+     * @param string $query   SQL query
+     * @return bool           Produced result
+     */
+    public function query( string $query ) : bool
+    {
+        return true;
+    }
+
+    /**
+     * Returns a single result from the last query
+     *
+     * @return array Query result
+     */
+    public function getResult() : array
+    {
+        return [];
+    }
+
+    /**
+     * Returns all results from the last query
+     *
+     * @return array Query results
+     */
+    public function getResults() : array
+    {
+        return [];
+    }
+
+    /**
+     * Get the auto incremented ID of the last INSERT operation
+     *
+     * @return int Row ID
+     */
+    public function getLastId() : int
+    {
+        return 0;
+    }
+
+    /**
+     * Closes the connection to the MySQL database
+     */
+    public function close() : void
+    {
+        return;
+    }
 }
