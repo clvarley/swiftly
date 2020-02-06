3dcd6151a6f079a9a9dc1c2db68c5b205a942691
clvarley <conorlukevarley@gmail.com>
2020-02-06 22:43:53 +0000

    Created stubs for the advanced routing framework

---
+++ src/swiftly/routing/AdvancedRouteLoader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Swiftly\Routing;
+
+/**
+ * Responsible for loading routes from files that use the advanced syntax
+ *
+ * @author C Varley <clvarley>
+ */
+Class AdvancedRouteLoader Implements RouteLoaderInterface
+{
+
+}
---
+++ src/swiftly/routing/AdvancedRouter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Swiftly\Routing;
+
+/**
+ * Advanced router that compiles given routes to regex
+ *
+ * @author C Varley <clvarley>
+ */
+Class AdvancedRouter Implements RouterAdapterInterface
+{
+
+}
