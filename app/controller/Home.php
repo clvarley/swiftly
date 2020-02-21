94c24e1fd6f9e8361f0164730d18fc135b506ade
clvarley <conorlukevarley@gmail.com>
2020-02-21 17:44:54 +0000

    Created a new temp homepage

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -17,20 +17,19 @@ Class Home Extends Controller
     public function index()
     {
 
-      // Get all the files in this Swiftly install
-      $swiftly_files = Directory::getFilesRecursive( APP_BASE );
+        // Get all the files in this Swiftly install
+        $swiftly_files = Directory::getFilesRecursive( APP_BASE );
 
-      // Filter out the .git folder
-      $swiftly_files = array_filter( $swiftly_files, function( $file ) {
-          return strpos( $file->getPath(), '/.git/' ) === false;
-      });
+        // Filter out the .git folder
+        $swiftly_files = array_filter( $swiftly_files, function( $file ) {
+            return strpos( $file->getPath(), '/.git/' ) === false;
+        });
 
-      // Output the response
-      return $this->setOutput($this->render('home', [
-          'title'   => 'Swiftly | A Simple Framework',
-          'message' => 'Thanks for installing Swiftly!',
-          'files'   => $swiftly_files
-      ]));
+        // Output the response
+        return $this->setOutput($this->render('home', [
+            'title'   => 'Swiftly',
+            'message' => 'Thanks for installing Swiftly!'
+        ]));
 
     }
 }
--- app/view/home.html.php
+++ app/view/home.html.php
@@ -1,60 +1,47 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title><?= $this->title ?></title>
-    <style>
-      body {
-        font-family: 'Segoe UI', Roboto, sans-serif;
-        font-size: 18px;
-        margin: 15px;
-        padding: 15px;
-        border: 1px solid #eaeaea;
-      }
-      h1 {
-        font-family: Calibri, Helvectia, sans-serif;
-        font-size: 28px;
-        margin: 0 0 10px 0;
-      }
-      p {
-        margin: 0 0 10px 0;
-      }
-      p:last-child {
-        margin: 0;
-      }
-      thead td {
-          font-weight: 700;
-      }
-      tbody td {
-          padding: 5px 2px 5px;
-          border-top: 1px solid #000000;
-      }
-    </style>
-  </head>
-  <body>
-
-    <h1><?= $this->title; ?></h1>
-    <p><?= $this->message; ?></p>
-
-    <?php if ( !empty($this->files) ): ?>
-        <table>
-            <thead>
-                <tr>
-                    <td>File</td>
-                    <td>Size</td>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach( $this->files as $file ): ?>
-                    <tr>
-                        <td><?= $file->getPath(); ?></td>
-                        <td><?= format_bytes($file->getSize()); ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    <?php endif; ?>
-
-  </body>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title><?= $this->title ?></title>
+        <style>
+            body {
+                font-family: 'Segoe UI', Roboto, sans-serif;
+                font-size: 18px;
+                height: 100vh;
+                margin: 0;
+                padding: 15px;
+                box-sizing: border-box;
+            }
+            .content {
+                position: relative;
+                border: 1px solid #eaeaea;
+                height: 100%;
+            }
+            .inner {
+                position: absolute;
+                top: 50%;
+                left: 15px;
+                right: 15px;
+                transform: translateY(-50%);
+                text-align: center;
+            }
+            h1 {
+                font-family: Calibri, Helvectia, sans-serif;
+                font-size: 28px;
+                margin: 0;
+            }
+            p {
+                margin: 15px 0 0 0;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="content">
+            <div class="inner">
+                <h1><?= $this->title; ?></h1>
+                <p><?= $this->message; ?></p>
+            </div>
+        </div>
+    </body>
 </html>
