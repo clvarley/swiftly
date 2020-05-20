10a9e2a44059c9c8ca6ea422cd423d9e5687ce59
clvarley <conorlukevarley@gmail.com>
2020-05-20 08:34:04 +0100

    Expanded bind methods

--- src/swiftly/dependencies/Container.php
+++ src/swiftly/dependencies/Container.php
@@ -18,19 +18,27 @@ Class Container
     private $services = [];
 
     /**
+     * Binds a service as a singleton
      *
+     * @param string $name    Service name
+     * @param callable|object Service implementation
+     * @return void           N/a
      */
     public function bindSingleton( string $name, $implementation ) : void
     {
-
+        $this->services[$name] = new Dependency( $implementation, true );
     }
 
     /**
+     * Binds a service
      *
+     * @param string $name    Service name
+     * @param callable|object Service implementation
+     * @return void           N/a
      */
     public function bindInstance( string $name, $implementation ) : void
     {
-
+        $this->services[$name] = new Dependency( $implementation, false );
     }
 
     /**
@@ -41,7 +49,12 @@ Class Container
      */
     public function resolve( string $name ) /* : ?object */
     {
+        $result = null;
 
+        if ( isset( $this->services[$name] ) ) {
+            $result = $this->services[$name]->resolve( $this );
+        }
+
+        return $result;
     }
-
 }
