1feb2b03e1d0be9115243d52918b1fad89e0f5ab
clvarley <conorlukevarley@gmail.com>
2020-12-21 16:36:08 +0000

    Final amends to autoloader implementation

--- src/Swiftly/utilities/Autoloader.php
+++ src/Swiftly/utilities/Autoloader.php
@@ -30,7 +30,7 @@ Final Class Autoloader
     public function addPrefix( string $prefix, string $path ) : void
     {
         if ( \is_dir( $path ) ) {
-            $this->prefixes[\mb_strtolower( $prefix )] = \rtrim( $path ) . '/';
+            $this->prefixes[\strtolower( $prefix )] = \rtrim( $path, '/' ) . '/';
         }
     }
 
@@ -47,37 +47,39 @@ Final Class Autoloader
 
         $parts = \explode( '\\', $class_name );
         $class = \array_pop( $parts );
+        $vendor = \array_shift( $parts );
 
         // Get class vendor
-        if ( !empty( $parts ) ) {
-            $vendor = \strtolower( \array_shift( $parts ) );
+        if ( !empty( $vendor ) ) {
+            $vendor = \strtolower( $vendor );
         } else {
             $vendor = '*';
         }
 
         // Vendor directory override?
         if ( !empty( $this->prefixes[$vendor] ) ) {
-            $dir_path = $this->prefixes[\strtolower( $vendor )];
+            $dir_path = $this->prefixes[ $vendor ];
         } else {
-            $dir_path = \get_include_path();
+            $dir_path = \get_include_path() . '/';
         }
 
         // Attempt 1. Simple file include?
-        if ( \is_file( $file = $dir_path . \implode( \DIRECTORY_SEPARATOR, $parts ) . "/$class.php" ) ) {
+        if ( \is_file( $file = $dir_path . \implode( '/', $parts ) . "/$class.php" ) ) {
             require $file;
             return;
         }
 
         // Atempt 2. Traverse directories
         foreach ( $parts as $part ) {
-            $dir_path .= $part . \DIRECTORY_SEPARATOR;
+            $dir_path .= $part . '/';
             if ( !\is_dir( $dir_path ) ) return; // Missing directory!
         }
 
         $files = \scandir( $dir_path, \SCANDIR_SORT_NONE );
+        $class = \strtolower( $class );
 
         foreach ( $files as $file ) {
-            if ( \mb_strtolower( \mb_substr( $file, 0, -4 ) ) === $class ) {
+            if ( \strtolower( \substr( $file, 0, -4 ) ) === $class ) {
                 require $dir_path . $file;
                 return;
             }
