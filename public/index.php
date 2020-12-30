b8ca9da68a774470dc0a358cc4f0d30f1d981cd8
clvarley <conorlukevarley@gmail.com>
2020-12-30 18:16:38 +0000

    Updated index to include polyfills

--- public/index.php
+++ public/index.php
@@ -27,6 +27,7 @@ if ( defined( 'SWIFTLY_MIN_PHP' ) && version_compare( PHP_VERSION, SWIFTLY_MIN_P
 
 // Load utility functions & autoloader
 require_once APP_SWIFTLY . 'utilities/functions.php';
+require_once APP_SWIFTLY . 'utilities/polyfills.php';
 require_once APP_SWIFTLY . 'utilities/Autoloader.php';
 
 
